8fb9e5595959a46e1cd3d1880610152c506b6587
ryomar-kappa <ryomaryoma777@yahoo.co.jp>
2024-11-03 00:16:09 +0900

    色定義

---
+++ lib/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color headerColor = Color(0xFFF1C40F); // ゴールデンイエロー
+  static const Color backgroundColor = Color(0xFFF7EED6); // クリーム色
+  static const Color textColor = Color(0xFF3E2723); // ダークブラウン
+  static const Color mainButtonColor = Color(0xFFD35400); // オレンジブラウン
+  static const Color buttonTextColor = Color(0xFFFFFFFF); // 白
+  static const Color correctAnswerColor = Color(0xFF4CAF50); // 緑
+  static const Color incorrectAnswerColor = Color(0xFFE74C3C); // 赤
+  static const Color quizBackground = Color(0xFFD7B68A);
+  static const Color quizText = Color(0xFF3E2723);
+}

14023a311a8edfeeaaae487d9025556d663466cb
ryomar-kappa <ryomaryoma777@yahoo.co.jp>
2024-11-03 00:16:19 +0900

    問題画面仮実装

--- lib/next_page/next_page.dart
+++ lib/next_page/next_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hop_client/app_color.dart';
 
 class NextPage extends StatelessWidget {
   const NextPage({super.key});
@@ -6,15 +7,107 @@ class NextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.headerColor,
+        title: Text(
+          '問題',
+          style: TextStyle(color: AppColor.textColor),
+        ),
+      ),
       body: SafeArea(
-        child: Column(
+        child: Container(
+          width: double.infinity,
+          color: AppColor.backgroundColor,
+          child: Column(
+            children: [
+              Expanded(
+                child: QuizArea(),
+              ),
+              ChoisesArea(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChoisesArea extends StatelessWidget {
+  const ChoisesArea({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Choises(
+          prefix: 'A',
+          choise: 'ホップ',
+        ),
+        Choises(
+          prefix: 'B',
+          choise: 'モルト',
+        ),
+        Choises(
+          prefix: 'C',
+          choise: '酵母',
+        ),
+        Choises(
+          prefix: 'D',
+          choise: '水',
+        ),
+      ],
+    );
+  }
+}
+
+class QuizArea extends StatelessWidget {
+  const QuizArea({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 16,
+        ),
+        Text('1.ビールの原材料のうち「ビールの魂」と呼ばれるものはどれ？'),
+      ],
+    );
+  }
+}
+
+class Choises extends StatelessWidget {
+  const Choises({super.key, required this.prefix, required this.choise});
+
+  final String prefix;
+  final String choise;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+            color: AppColor.quizBackground,
+            borderRadius: BorderRadius.all(Radius.circular(16))),
+        child: Row(
           children: [
-            Text('次のページ'),
-            FilledButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('戻る'))
+            Container(
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(16)),
+              child: Text(prefix),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                choise,
+                style: TextStyle(color: AppColor.quizText),
+              ),
+            )
           ],
         ),
       ),

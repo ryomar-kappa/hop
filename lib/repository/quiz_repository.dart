ec3f067c91d1e94780432bfd49c16ff56498d8c9
ryomar-kappa <ryomaryoma777@yahoo.co.jp>
2024-11-23 23:50:04 +0900

    不要なコメント削除

--- lib/repository/quiz_repository.dart
+++ lib/repository/quiz_repository.dart
@@ -4,18 +4,13 @@ import 'package:flutter/services.dart';
 import 'package:hop_client/model/quiz.dart';
 
 class QuizRepository {
-  // JSONファイルのパス
   static const String _jsonFilePath = 'assets/quiz_data.json';
 
-  // JSONデータを非同期に読み込む
   Future<List<Quiz>> fetch() async {
-    // アセットからJSONファイルを読み込む
     final String jsonString = await rootBundle.loadString(_jsonFilePath);
 
-    // JSONをデコードしてリストに変換
     final List<dynamic> jsonData = json.decode(jsonString);
 
-    // JSONからQuizオブジェクトを作成
     List<Quiz> quizzes = [];
     for (var item in jsonData) {
       quizzes.add(Quiz(

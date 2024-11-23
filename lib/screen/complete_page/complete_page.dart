8779e077d0c2507b2fd590c8719b23285a5555ab
ryomar-kappa <ryomaryoma777@yahoo.co.jp>
2024-11-23 23:19:43 +0900

    ホーム画面に戻る挙動を修正

--- lib/screen/complete_page/complete_page.dart
+++ lib/screen/complete_page/complete_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hop_client/main.dart';
 
 class CompletePage extends StatelessWidget {
   @override
@@ -11,7 +12,12 @@ class CompletePage extends StatelessWidget {
             Center(
               child: FilledButton(
                   onPressed: () {
-                    Navigator.popUntil(context, (route) => route.isFirst);
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MainApp()), // ホーム画面への遷移
+                      (Route<dynamic> route) => false, // スタック上の他のすべての画面を削除
+                    );
                   },
                   child: Text('ホーム画面に戻る')),
             )
